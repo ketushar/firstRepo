83b65315802c14818d729ac31feb41099decd9c2
ketushar <kanchan.d410@gmail.com>
2022-02-07 12:09:36 +0530

    this id thirdDemo js file

---
+++ thirdDemo.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<title>W3.JS</title>
+<script src="https://www.w3schools.com/lib/w3.js"></script>
+
+<body>
+
+<button onclick="w3.hide('p')">Hide all p elements</button>
+
+<h2>London</h2>
+<p>London is the capital city of England.</p>
+<p>It is the most populous city in the United Kingdom.</p>
+<p>Population over 13 million inhabitants.</p>
+
+</body>
+</html>
